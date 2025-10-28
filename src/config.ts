46c515030a93372abb22ae8ba3df1e6cf4cf174d
fufu <2741835851@sjtu.edu.cn>
2025-10-28 23:40:42 +0800

    feat(settings): set up umami

--- src/config.ts
+++ src/config.ts
@@ -544,10 +544,11 @@ export const widgetConfigs = {
 } as const;
 
 export const umamiConfig = {
-  enabled: false, // 是否显示Umami统计
-  apiKey: import.meta.env.UMAMI_API_KEY || "api_xxxxxxxx", // API密钥优先从环境变量读取，否则使用配置文件中的值
+  enabled: true, // 是否显示Umami统计
+  apiKey:
+    import.meta.env.UMAMI_API_KEY || "api_p8ibr9MESWUxFMNQxKk5WRXgFMlI71sj", // API密钥优先从环境变量读取，否则使用配置文件中的值
   baseUrl: "https://api.umami.is", // Umami Cloud API地址
   scripts: `
-<script defer src="XXXX.XXX" data-website-id="ABCD1234"></script>
+<script defer src="https://cloud.umami.is/script.js" data-website-id="a2eabb15-f42f-4feb-aefa-675649d03f90"></script>
   `.trim(), // 上面填你要插入的Script,不用再去Layout中插入
 } as const;
